894b2a91207b8f6e6c6d22ff5f889eb6ff51d938
Bhanu <bhanukishore29@yahoo.in>
2019-08-18 14:24:43 +0530

    Latest UI first commit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import AdminLayout from "layouts/Admin/Admin.jsx";
-import RTLLayout from "layouts/RTL/RTL.jsx";
-import Login from "layouts/Login/Login.jsx";
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import "assets/scss/black-dashboard-react.scss";
-import "assets/demo/demo.css";
-import "assets/css/nucleo-icons.css";
-import "assets/css/fys.css";
-
-const hist = createBrowserHistory();
-
-ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/main" render={props => <AdminLayout {...props} />} />
-      <Route path="/rtl" render={props => <RTLLayout {...props} />} />
-      <Route path="/login" render={props => <Login />} />
-      <Redirect from="*" to="/login/signIn" />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/localSorage.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  setLocal: function(item, value){
-    localStorage.setItem(item,value);
-  },
-  getLocal: function(item){
-    localStorage.getItem(item);
-  }
-}
